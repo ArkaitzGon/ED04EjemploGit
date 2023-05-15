d76dbd11bdcf04a3e5c1d361d7cdd9642eae259e
Arkaitz <agonzalez@argonzalez@birt.eus>
2023-05-15 23:11:27 +0200

    Version 0. Primer commit del proyecto

---
+++ src/main/java/com/mycompany/ud04ejemplogit/EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud04ejemplogit;
+
+/**
+ *
+ * @author Arkaitz
+ */
+public class EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
